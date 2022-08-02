c5ed386ca1770fa4a6125546461741ff316201fc
potential701 <viktors.makreckis@cygnia.net>
2022-08-02 15:10:29 +0100

    dd

---
+++ FirstApp_DataAccess/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp_DataAccess
+{
+	internal class Category
+	{
+	}
+}
